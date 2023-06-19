d3ef2e624c8e236a38971b36c5d6fca6946c2f16
anastasiya <a.koush@ya.ru>
2023-06-19 11:46:50 +0300

    Refactored

--- app/src/main/java/com/example/lab_1/MainActivity.kt
+++ app/src/main/java/com/example/lab_1/MainActivity.kt
@@ -29,53 +29,6 @@ import com.example.lab_1.ui.theme.Lab_1Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Lab_1Theme {
-                Column(
-                    Modifier
-                        .fillMaxHeight()
-                        .fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        modifier = Modifier.padding(vertical = 20.dp),
-                        text = stringResource(R.string.FIO),
-                        color = Color.Blue,
-                        style = TextStyle(
-                            fontSize = 35.sp,
-                            textAlign = TextAlign.Center
-                        )
-                    )
-                    Text(
-                        modifier = Modifier.padding(vertical = 20.dp),
-                        text = stringResource(R.string.speciality),
-                        color = Color.Blue,
-                        style = TextStyle(
-                            fontSize = 25.sp,
-                            textAlign = TextAlign.Center
-                        )
-                    )
-                    Text(
-                        modifier = Modifier.padding(vertical = 20.dp),
-                        text = stringResource(R.string.group),
-                        color = Color.Blue,
-                        style = TextStyle(
-                            fontSize = 15.sp,
-                            textAlign = TextAlign.Center
-                        )
-                    )
-
-                    Spacer(modifier = Modifier.weight(1f))
-
-                    var isPortrait =
-                        LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT
-
-                    Text(
-                        modifier = Modifier.padding(all = 15.dp),
-                        text = if (isPortrait) "Portrait view" else "Landscape view"
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.main_activity)
     }
 }
